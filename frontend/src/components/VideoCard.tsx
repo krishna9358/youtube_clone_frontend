45500853785e889feb17dd5cbd605d0bd7f80cad
Krishna Mohan <krishanmohank974@gmail.com>
2024-01-23 04:44:15 +0530

    started making components, VideoCard is on editing mode

---
+++ frontend/src/components/VideoCard.tsx
@@ -0,0 +1,13 @@
+export  function VideoCard(){
+    return <div>
+        <img src="/thumbnail.png" alt="a"  />
+        <div className="grid grid-cols-12 bg-red-900">
+            <div className="col-span-2 ">
+                <img className="rounded-full w-10 h-10" src="/thumbnail.png" alt="a"  />
+            </div>
+            <div className="col-span-10"> 
+                hello me hu krishna
+            </div>
+        </div>
+    </div>
+}
