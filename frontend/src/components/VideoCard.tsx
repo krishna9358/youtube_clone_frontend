cfb871921256b7bb642480273a98e629abaa933a
Krishna Mohan <krishanmohank974@gmail.com>
2024-01-23 15:12:26 +0530

    made the component dynamic, index is still generic

--- frontend/src/components/VideoCard.tsx
+++ frontend/src/components/VideoCard.tsx
@@ -1,19 +1,19 @@
-export  function VideoCard(){
+export  function VideoCard(props:any){
     return <div>
-        <img src="/thumbnail.png" alt="a"  className="rounded-lg"/>
+        <img src={props.image} alt="a"  className="rounded-lg"/>
         <div className="grid grid-cols-12 pt-2 ">
             <div className="col-span-1 ">
-                <img className="rounded-full  w-12 h-12 " src="/thumbnail.png" alt="a"  />
+                <img className="rounded-full  w-12 h-12 " src={props.channel} alt="a"  />
             </div>
             <div className="col-span-11 "> 
                 <div className="text-lg">
-                Thumbnail title 
+                {props.title}
                 </div>
             <div className="col-span-11 text-gray-600 text-sm"> 
-               Krishna Mohan
+               {props.author}
             </div>
             <div className="col-span-11  text-gray-600 text-sm"> 
-                100M  | 1 day ago 
+               {props.views} | {props.timestamp}
             </div>
                 
             </div>
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,13 +1,20 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import {VideoCard} from "@/components/VideoCard";
+import { timeStamp } from "console";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
       Hello there
-      <VideoCard></VideoCard>
+      <VideoCard title={"Just trying youtube clone | Using typescript"}
+      image={"thumbnail.png"}
+      channel={"thumbnail.png"}
+      timestamp={"1 day ago"}
+      views= {"100M"}
+      author={"Krishna Mohan"}
+      ></VideoCard>
     </div>
    );
 }
