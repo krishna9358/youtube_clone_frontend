0eeb7d682ebaf29fdf827f0278c1844c9ab6b502
Krishna Mohan <krishanmohank974@gmail.com>
2024-01-23 15:01:27 +0530

    made a videocard component, created readme

--- frontend/src/components/VideoCard.tsx
+++ frontend/src/components/VideoCard.tsx
@@ -1,12 +1,21 @@
 export  function VideoCard(){
     return <div>
-        <img src="/thumbnail.png" alt="a"  />
-        <div className="grid grid-cols-12 bg-red-900">
-            <div className="col-span-2 ">
-                <img className="rounded-full w-10 h-10" src="/thumbnail.png" alt="a"  />
+        <img src="/thumbnail.png" alt="a"  className="rounded-lg"/>
+        <div className="grid grid-cols-12 pt-2 ">
+            <div className="col-span-1 ">
+                <img className="rounded-full  w-12 h-12 " src="/thumbnail.png" alt="a"  />
             </div>
-            <div className="col-span-10"> 
-                hello me hu krishna
+            <div className="col-span-11 "> 
+                <div className="text-lg">
+                Thumbnail title 
+                </div>
+            <div className="col-span-11 text-gray-600 text-sm"> 
+               Krishna Mohan
+            </div>
+            <div className="col-span-11  text-gray-600 text-sm"> 
+                100M  | 1 day ago 
+            </div>
+                
             </div>
         </div>
     </div>
